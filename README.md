88ddfb0bd22fc59fcdf54d7eca3f3c72b314de7e
Scott Gopaulchan <scott.gopaulchan@diagonconsulting.com>
2023-03-21 11:33:38 -0400

    stable update

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Python Thumbnail Generator
+Python Thumbnail Generator is a simple program that helps you create thumbnails for your images in just a few lines of code. This program uses the Python Imaging Library (PIL) to manipulate images and create thumbnails.
+
+## Prerequisites
+
+1. ) Python 3.6 or higher
+2. ) PIL (Python Imaging Library)
+
+## Installation
+Clone this repository: `git clone https://github.com/itachi1621/PythonThumbnailGenerator.git`
+Install PIL:```
+Copy code pip install pillow```
+
+## Usage
+1.) Open your terminal and navigate to the directory where you have cloned this repository.
+2.) Run `python thumbnail_generator.py -h` to see the list of available commands and options.
+
+```Copy code 
+usage: thumbnail_generator.py [-h] [--width WIDTH] [--height HEIGHT] input_dir output_dir
+
+Create thumbnails for images in a directory.
+
+positional arguments:
+  input_dir        Input directory containing images to be used for thumbnails
+  output_dir       Output directory for thumbnails to be placed
+
+options:
+  -h, --help       show this help message and exit
+  --width WIDTH    Width of the thumbnail
+  --height HEIGHT  Height of the thumbnail
+
+```
+3.) To create a thumbnail, `run python thumbnail_creator.py  /path/to/images /path/to/thumbnails` .
+This command will create thumbnails for all images in the /path/to/images directory as well as in any subdirectories.
+
+
+## License
+This program is licensed under the MIT License. See the LICENSE file for details.
--- thumbnail_generator.py
+++ thumbnail_generator.py
@@ -1,27 +1,54 @@
 from PIL import Image;
 import os;
+import argparse;
 
 #Check if thumbnails directory exsists
-if not os.path.exists('thumbnails'):
-    os.makedirs('thumbnails');
 
-def createThumbnails(image_list,width=128,height=128):
+def outputDirectoryCheck(path):
+    if not os.path.exists(path):
+        os.makedirs(path);
+    else:
+        pass;
+        #print("Directory already exsists");
+    
+
+
+def createThumbnails(image_list,thumbnail_path='thumbnails',width=200,height=200):
+    outputDirectoryCheck(thumbnail_path);
+    imgamount=str(len(image_list))
     for image in image_list:
         im = Image.open(image);
         im.thumbnail((width,height));
-        im.save('thumbnails/'+image);
+        #get image name for placement in the thumbnail directory
+        image_name=os.path.basename(image)
+        im.save(os.path.join(thumbnail_path,image_name));
+        print('Created thumbnail for '+image_name+' '+str(image_list.index(image)+1)+'/'+imgamount);
+        # might modify this to be user defined later or default to thumbnails
         
-def findImages(path):
+def findAllImages(path):#Finds all supported images in a given directory including subfolders
     image_list = [];
-    image_type_list = []
-    for root,dirs,files in os.walk(path):
+
+    pillow_exts = Image.registered_extensions()#Provides a list that pillow supports
+    supported_extensions = {ex for ex, f in pillow_exts.items() if f in Image.OPEN}#Filter down list to those who have open feature 
+    #print(supported_extensions);
+    for root, dirs, files in os.walk(path):
         for file in files:
-            if file.endswith('.jpg'):
-                image_list.append(file);
+            #Check if current file ends with a supported extension
+            if file.endswith(tuple(supported_extensions)):#Only accept files with supported extensions
+                image_list.append(os.path.join(root,file));
+            
     return image_list;
-        
-img_list=["sample.jpg"]
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create thumbnails for images in a directory.')
+    parser.add_argument('input_dir', help='Input directory containing images to be used for thumbnails')
+    parser.add_argument('output_dir', help='Output directory for thumbnails to be placed')
+    parser.add_argument('--width', type=int, default=200, help='Width of the thumbnail')
+    parser.add_argument('--height', type=int, default=200, help='Height of the thumbnail')
 
+    args=parser.parse_args()
 
-createThumbnails(img_list);
+    img_list = findAllImages(args.input_dir);
+    print('Found '+str(len(img_list))+' images')
+    #print('Found '.len(img_list) + ' images ');
+    createThumbnails(img_list,args.output_dir,args.width,args.height);
